1eda335c22bd31dcd3ff2ea94039b168fbf07b3f
erk1nqz <221326@astanait.edu.kz>
2023-10-23 03:46:32 +0600

    js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var nav = document.querySelector('nav');
-var logo = document.querySelector('.navbar-brand')
+let nav = document.querySelector('nav');
+let logo = document.querySelector('.navbar-brand')
 const navLinks = document.querySelectorAll('.nav-link')
 
 window.addEventListener('scroll', function() {
@@ -17,5 +17,45 @@ window.addEventListener('scroll', function() {
             navLink.classList.remove('text-black')
             navLink.classList.add('text-white');
         });
+    }
+})
+
+let flowers = [];
+
+window.addEventListener('click', function(event) {
+    if (event.target.dataset.action === "flower-plus") {
+        const option = event.target.closest('.option');
+        const flower = option.querySelector('.option-text').innerText;
+        flowers.push(flower);
+    } else if (event.target.dataset.action === "flower-minus") {
+        const option = event.target.closest('.option');
+        const flower = option.querySelector('.option-text').innerText;
+        let index = flowers.indexOf(flower);
+        if (index > -1) {
+            flowers.splice(index, 1);
+        }
+    }
+    const cart = this.document.querySelector('.cart');
+    let order = this.document.querySelector(".order-now");
+    order.href = "https://api.whatsapp.com/send?phone=77762090592&text=Здравствуйте!%20Хочу%20заказать%20у%20вас%20букет%20с%20таким%20содержимым:";
+    if (flowers.length != 0) {
+        cart.innerHTML = ""
+    }
+    let counter = {};
+    for (let i = 0; i < flowers.length; i++) {
+        let temp = flowers[i];
+        if (counter[temp] != undefined) {
+            ++counter[temp];
+        } else {
+            counter[temp] = 1;
+        }
+    }
+    for (let flower in counter) {
+        const paragraph = this.document.createElement('p')
+        let order = this.document.querySelector(".order-now");
+        order.href += `%20${flower}%20${counter[flower]}%20шт,`
+        const node = this.document.createTextNode(`${flower} ${counter[flower]}`);
+        paragraph.appendChild(node);
+        cart.appendChild(paragraph);
     }
 })
